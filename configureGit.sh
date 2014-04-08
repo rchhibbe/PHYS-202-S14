37e583c978708a64e72267d62ad3d70356f4b5ec
Raghav Chhibber <rchhibbe@calpoly.edu>
2014-04-08 14:11:27 -0700

    adding files from week1

---
+++ configureGit.sh
@@ -0,0 +1,4 @@
+#this scrip configures git whenever you start working on multiple machines
+git config --global user.name "Raghav Chhibber"
+git config --global user.email "rchhibbe@calpoly.edu"
+git config --global core.editor "nano"
